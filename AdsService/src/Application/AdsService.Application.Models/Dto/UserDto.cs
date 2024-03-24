578a511d06073033003140eaeb6e4ca96c64881b
AngryRectangle <yashmel2000@gmail.com>
2024-03-24 14:36:06 +0300

    feat: adapt entities to ef core

--- AdsService/src/Application/AdsService.Application.Models/Dto/UserDto.cs
+++ AdsService/src/Application/AdsService.Application.Models/Dto/UserDto.cs
@@ -2,4 +2,4 @@ namespace AdsService.Application.Models.Dto;
 
 using AdsService.Application.Models.ValueObjects;
 
-public record UserDto(Guid Id, Name Name, Balance Balance);
+public record UserDto(Guid Id, Name Name, Money Balance);
